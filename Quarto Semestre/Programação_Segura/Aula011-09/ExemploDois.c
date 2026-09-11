e7d965528f31fd887f79e82dccb9be94611a309f
Murilo Mattos <142946973+BlessedByJah@users.noreply.github.com>
2026-09-11 20:34:34 -0300

    Codigo completo

--- Quarto Semestre/Programação_Segura/Aula011-09/ExemploDois.c
+++ Quarto Semestre/Programação_Segura/Aula011-09/ExemploDois.c
@@ -1,4 +1,7 @@
 # include <stdio.h>
+# include <stdlib.h>
+#define TAMANHO 100
+#define CHAVE 3
 
 void cifrar(char 8text, int chave)
 {
@@ -17,17 +20,57 @@ void cifrar(char 8text, int chave)
               text[i] = 'A' + (texto[i] - 'A' + chave) % 26;
             }
         }
+        i = i + 1; 
     }
 }
 
 void decifrar( char *texto, int chave)
 {
+    i = 0;
 
+    while( tetxo[i] != '\0')
+    {
+        if ( text[i] >= 'a' && texto[i] <= 'z')
+        {
+            text[i] = 'a' + (texto[i] - 'a' - chave) % 26;
+        }
+        else
+        {
+            if (texto[i] >= 'A' && texto[i] <= 'Z')
+            {
+              text[i] = 'A' + (texto[i] - 'A' - chave) % 26;
+            }
+        }
+    }
 }
 
-int main()
+int main( int argc, char *argv[])
 {
+    chr mensagem[TAMANHO];
+    int chave = CHAVE;
 
+    if(argc < 2)
+    {
+        printf("USAGE: %s \"mensagem\" [key]\n", argv[0]); 
+        retunr EXIT_FAILURE;
+    }
 
-return 0;
+    if(argv > 2)
+    {
+        chave = atoi(argv[2]);
+    }
+
+    if (chave < 0 || chave > 25)
+    {
+        printf("ERRO: Chave deve ser um valor entre 0-25");
+        return EXIT_FAILURE;
+    }
+
+    printf("Chave . . . . . . . . . . . . .: %d\n", chave);
+    printf("Mensagem Original . . . . . . .: %s\n", mensagem);
+    cifrar(mensagem, chave);
+    printf("Mensagem Cifrada  . . . . . . .: %s\n", mensagem);
+    decifrar(mensagem, chave);
+    printf("mensagem Decifrada. . . . . . .: %s\n", mensagem);
+    return EXIT_SUCCESS;
 }
